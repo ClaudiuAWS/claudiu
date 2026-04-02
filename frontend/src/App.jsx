24df4d6f5228a5cd1475345a338f5365b012a9ec
Robert076 <robertberes06@gmail.com>
2026-04-02 12:49:37 +0300

    feat: testing cicd

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import BottomNav from "./components/BottomNav";
 export default function App() {
   return (
     <div className="min-h-screen bg-gray-950 pb-24">
-      <h1>Hello AWS</h1>
+      <h1>Hello AWS!</h1>
       <BottomNav />
     </div>
   )
